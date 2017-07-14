910cdce57136b32e6f9908754dd7bfb552f30745
bvin <bvin@live.cn>
2017-07-14 11:37:08 +0800

    progress demo

--- demo/src/main/java/cn/bvin/android/lib/widget/waveview/demo/MainActivity.java
+++ demo/src/main/java/cn/bvin/android/lib/widget/waveview/demo/MainActivity.java
@@ -2,6 +2,9 @@ package cn.bvin.android.lib.widget.waveview.demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import cn.bvin.android.lib.widget.waveview.WaveView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final WaveView waveView = (WaveView) findViewById(R.id.wv);
+
+        new Thread(){
+            @Override
+            public void run() {
+                super.run();
+                for (int i = 1; i <= 100; i++) {
+                    final float person = i / 100f;
+                    Log.d("onCreate: ",""+person);
+                    try {
+                        Thread.sleep(200);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            waveView.setPercent(person);
+                        }
+                    });
+                }
+            }
+        }.start();
     }
 }
